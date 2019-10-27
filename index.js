d73c661996fdd9efb7faa2933f623f50f8a8b3a8
laggingreflex <laggingreflex@gmail.com>
2019-10-27 10:01:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const esm = require("@fpipita/esm-middleware");
+const path = require("path");
+
+const app = express();
+/**
+ * esm-middleware needs be loaded before any static middleware
+ * in order for the module import specifiers to be processed
+ */
+app.use(esm());
+
+app.use("/node_modules", express.static(path.resolve("node_modules")));
+app.use(express.static(path.resolve("client")));
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
